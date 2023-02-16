1a549ff23710772bd9b8d36170f9860ddff55067
TuanNguyen2204 <tuanht2204201@gmail.com>
2023-02-16 21:01:57 +0700

    add user avatar chat UI

--- ChatApp-SignalR/ChatApp-SignalR/CustomControls/StatusThumbs.xaml.cs
+++ ChatApp-SignalR/ChatApp-SignalR/CustomControls/StatusThumbs.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatApp_SignalR.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ ChatApp-SignalR/ChatApp-SignalR/ViewModels/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using ChatApp_SignalR.CustomControls;
+using ChatApp_SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_SignalR.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<StatusDataModel> statusThumbsCollection { get; set; }   
+
+        public ViewModel()
+        {
+            statusThumbsCollection = new ObservableCollection<StatusDataModel>()
+            {
+                //Since we want to keep first status blank for the user to add own status
+                new StatusDataModel
+                {
+                    IsMeAddStatus = true
+                },
+                new StatusDataModel
+                {
+                    ContactName = "Charlie",
+                    ContactPhoto = new Uri("/assets/1.png"),
+                    StatusImage = new Uri("/assets/5.png"),
+                    IsMeAddStatus = false
+                }, new StatusDataModel
+                {
+                    ContactName = "Matthew",
+                    ContactPhoto = new Uri("/assets/2.png"),
+                    StatusImage = new Uri("/assets/5.png"),
+                    IsMeAddStatus = false
+                }, new StatusDataModel
+                {
+                    ContactName = "Elwyn",
+                    ContactPhoto = new Uri("/assets/3.png"),
+                    StatusImage = new Uri("/assets/5.png"),
+                    IsMeAddStatus = false
+                },
+            };
+            OnPropertyChanged("statusThumbsCollection");
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
